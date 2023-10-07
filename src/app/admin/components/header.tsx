c362b9de18769705554fc13bc412ee28a3d61c37
david <david@pop-os.localdomain>
2023-10-07 18:48:35 -0500

    feature(component): head layout admin view implemented

--- src/app/admin/components/header.tsx
+++ src/app/admin/components/header.tsx
@@ -1,7 +1,17 @@
+'use client'
+
+import { AvatarIcon } from "@/components/icons/avatar"
+import { useState } from "react"
+
 export const HeaderAdmin = () => {
 
+    const [companyName, setCompanyName] = useState<string>('Company')
 
-    return <div>
-        
+    return <div className="h-[90px] mb-4 rounded flex justify-end font-bold items-center gap-2 shadow bg-white text-black px-10">
+        {companyName}
+        <div className="border bg-[--main-yellow] w-10 h-10 grid place-content-center rounded-full">
+            <AvatarIcon />
+        </div>
+
     </div>
 }
