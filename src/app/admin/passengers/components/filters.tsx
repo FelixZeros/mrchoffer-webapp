92bea5c46d5fd196886d18cde2eb6b897003ccb0
david <david@pop-os.localdomain>
2023-10-08 09:18:39 -0500

    feature(passengers-view): basic layout of passenger view

---
+++ src/app/admin/passengers/components/filters.tsx
@@ -0,0 +1,31 @@
+
+
+export const FiltersPassengers = () => {
+
+
+    return <div className='flex justify-between my-4'>
+
+    <div className="flex items-center gap-2"> 
+
+    <input id='selectAll' type="checkbox" />    
+    <label htmlFor="#selectAll" className="text-black">Seleccionar todo</label>
+        
+    </div>
+
+    <div className='flex gap-1'>
+
+      <select className=' shadow p-2 rounded-lg bg-white text-black'>
+        <option value=''>Género</option>
+      </select>
+
+      <select className=' shadow p-2 rounded-lg bg-white text-black'>
+        <option value=''>No. Carreras</option>
+      </select>
+
+      <button className='bg-[--main-yellow] px-5 rounded-lg font-bold shadow text-black'>
+        Filtrar
+      </button>
+    </div>
+  </div>
+
+}
