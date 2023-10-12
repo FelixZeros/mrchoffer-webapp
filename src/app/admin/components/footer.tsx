388823986c4cd2688008706f873a8865d7b33f5a
Heli Cadena <heli.cadena@devzeros.com>
2023-10-12 14:18:22 -0500

    layout Hc update 2.0

--- src/app/admin/components/footer.tsx
+++ src/app/admin/components/footer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="w-full p-0 bg-[--main-yellow] text-center absolute bottom-0 text-black py-1">
-      <p className="text-xs w-full">20023 TODOS LOS DERECHOS RESERVADOS</p>
+    <div className="w-full bg-[--main-yellow] text-center fixed bottom-0 text-black py-1 ">
+      <p className="text-xs">2023 TODOS LOS DERECHOS RESERVADOS</p>
     </div>
   );
 };
--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -7,6 +7,7 @@ import {useSession, useSupabaseClient} from '@supabase/auth-helpers-react'
 import {AsideBar} from './components/asidebar'
 import {HeaderAdmin} from './components/header'
 import Footer from "@/app/admin/components/footer";
+import {Arow} from "@/components/icons/arow";
 
 const RootLayout: FC<PropsWithChildren> = ({children}) => {
   const supabase = useSupabaseClient()
@@ -20,11 +21,9 @@ const RootLayout: FC<PropsWithChildren> = ({children}) => {
       router.replace('/')
     }
   })
-
   const signOut = async () => {
     mutate()
   }
-
   const session = useSession()
 
   useEffect(() => {
@@ -41,57 +40,34 @@ const RootLayout: FC<PropsWithChildren> = ({children}) => {
     setIsVisible(!isVisible);
   }
   const [isVisible, setIsVisible] = useState(true);
-
-
   return (
-    <>
-      <div className="flex flex-row">
-        <div
-          className={`${isVisible ? "w-1/5" : "hidden"} container content-center`}>
-
-          <AsideBar isVisible={isVisible}/>
-        </div>
-        <div style={isVisible ? {left: "20%"} : {}}
-             className="h-full absolute flex flex-col top-0 left-0  justify-center items-center">
-          <button
-
-            className="w-6 h-1/6 bg-[#181818] rounded-tr-full rounded-br-full self-center"
-            onClick={toggleAside}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                 viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-                 className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round"
-                    d="M15.75 19.5L8.25 12l7.5-7.5"/>
-            </svg>
-
-          </button>
-        </div>
-        <div className={`${isVisible ? "w-4/5" : "w-full"}`}>
-
-          <section className="w-full">
-            <div className="p-4 pl-8">
-
-              <HeaderAdmin/>
-              <div className="w-full p-4 shadow rounded-lg">
-                {children}
-              </div>
-            </div>
-            <div>
-
-              <Footer></Footer>
-            </div>
-
-
-          </section>
-
-        </div>
+    <div className="flex flex-row">
+      <div
+        className={`${isVisible ? "w-1/5" : "hidden"} container content-center`}>
+        <AsideBar isVisible={isVisible}/>
       </div>
-      <div>
+      <div style={isVisible ? {left: "20%"} : {}}
+           className="h-full absolute flex flex-col top-0 left-0  justify-center items-center">
+        <button
+          className="w-6 h-1/6 bg-[#181818] rounded-tr-full rounded-br-full self-center"
+          onClick={toggleAside}
+        >
+          <Arow/>
+        </button>
       </div>
-
-
-    </>
+      <div className={`${isVisible ? "w-4/5" : "w-full"}`}>
+        <section className="w-full h-full">
+          <div className="p-4 pl-8 ">
+            <HeaderAdmin/>
+            <div
+              className="w-full p-4 shadow rounded-lg flex items-center justify-center">
+              {children}
+            </div>
+          </div>
+            <Footer></Footer>
+        </section>
+      </div>
+    </div>
   )
 }
 
