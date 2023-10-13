52a1bfc55f46bf0990735f9c44e4dff02739838d
David <davidagamez04@gmail.com>
2023-10-13 07:53:56 -0500

    feature(drivers): filters implemented only visual

--- src/app/admin/components/filters.tsx
+++ src/app/admin/components/filters.tsx
@@ -1,4 +1,4 @@
-export const Filters = () => {
+export const FiltersGenerics = () => {
   return (
     <div className='flex justify-between my-4'>
       <div>
@@ -9,7 +9,7 @@ export const Filters = () => {
         />
       </div>
 
-      <div className='flex gap-1'>
+      <div className='flex gap-3'>
         <select className=' shadow p-2 rounded-lg bg-white text-black'>
           <option value=''>Ciudad</option>
         </select>
@@ -24,6 +24,30 @@ export const Filters = () => {
 
         <select className=' shadow p-2 rounded-lg bg-white text-black'>
           <option value=''>No. Carreras</option>
+        </select>
+
+        <button className='bg-[--main-yellow] px-5 rounded-lg font-bold shadow text-black'>
+          Filtrar
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export const BlockedFilters = () => {
+  return (
+    <div className='flex justify-between my-4'>
+      <div>
+        <input
+          type='text'
+          className='rounded-lg bg-white shadow p-2'
+          placeholder='Buscar...'
+        />
+      </div>
+
+      <div className='flex gap-3'>
+        <select className=' shadow p-2 rounded-lg bg-white text-black'>
+          <option value=''>Ciudad</option>
         </select>
 
         <button className='bg-[--main-yellow] px-5 rounded-lg font-bold shadow text-black'>
